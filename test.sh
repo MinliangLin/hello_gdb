94d1291d4a91d148b3c45c6ee349d9040cf09512
MinliangLin <minliang.lin@hotstar.com>
2023-05-07 16:49:33 +0800

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
+echo %% TEST MAIN
 gdb -ex 'b main.c:7' -ex q build/main.out
-gdb -ex 'b foo.c:4' -ex q build/main.out
+
+echo %% TEST shared lib
+gdb -ex 'set confirm off' -ex 'b foo' -ex r -ex q build/main.out
